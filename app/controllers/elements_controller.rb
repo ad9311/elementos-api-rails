ccd72b92d45deca48ce0cfd8438cd130a5acce8f
Ángel Díaz <diazangel9311@gmail.com>
2023-08-22 23:09:42 -0500

    Refactor index.json.jbuilder file

--- app/controllers/elements_controller.rb
+++ app/controllers/elements_controller.rb
@@ -1,17 +1,5 @@
 class ElementsController < ApplicationController
   def index
     @elements = Element.order(atomic_number: :asc)
-    attributes_to_exclude = ['id', 'created_at', 'updated_at']
-    export_data = @elements.map do |element|
-      data = element.attributes.except(*attributes_to_exclude)
-      data['name'] = element.name.downcase
-      data['symbol'] = element.symbol.downcase
-      data['group'] = element.group&.to_i
-      data
-    end
-
-    File.open('elements.json', 'w') do |file|
-      file.write(JSON.pretty_generate({ elements: export_data }))
-    end
   end
 end
